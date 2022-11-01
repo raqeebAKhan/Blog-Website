423749c37c2236453ebeca15d0c0fef4eaa5cfef
raqeebAKhan <ritzyraqeeb@gmail.com>
2022-11-01 18:05:02 +0530

    first commit

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -1,26 +1,58 @@
 const express = require('express')
 const app = express();
-
-const articleInfo = {
-    "travel-work":{ 
-    comments: [],
-    },
-    "travel-Photo":{ 
-        comments: [],
-        },
-        "travel-beach":{ 
-            comments: [],
-            },
-}
+const { MongoClient} = require("mongodb")
 
 app.use(express.json())
+
+
+
+const withDB = async (operations,res) => {
+    try{
+    const client = await MongoClient.connect('mongodb://localhost:27017',{
+        useNewUrlParser : true,
+        useUnifiedTopology : true,
+})
+    const db = client.db("myblog")
+    await operations(db)
+    client.close();
+    } 
+    catch(error){
+        // res.status(500).json({message : 'Error connecting to db', error})
+    }
+}
+
+app.get("/api/articles/:name", async(req, res) => {
+   withDB(async(db) =>{
+    const articleName = req.params.name;
+    const articleInfo = await db
+    .collection("articles")
+    .findOne({ name: articleName });
+    res.status(200).json(articleInfo);
+    client.close();
+   },)
+   
+    } 
+)
 
 app.post("/api/articles/:name/add-comments", (req, res) => {
     const { username, text } = req.body;
     const articleName = req.params.name;
 
-    articleInfo[articleName].comments.push({ username, text});
-    res.status(200).send(articleInfo[articleName])
+    withDB(async(db) => {
+        const articleInfo = await db.collection("articles").findOne({
+            name: articleName
+        })
+        await db.collection('articles').updateOne({ name: articleName}, {
+            $set : {
+                comments: articleInfo.comments.concat({ username, text}),
+            }
+        })
+        const updateArticleInfo = await db
+        .collection("article").findOne({ name: articleName});
+        res.status(200).json(updateArticleInfo)
+    }, res)
+
+    
 })
 
-app.listen(3000, () => console.log("Server 3000"))
+app.listen(8000, () => console.log("Server 8000"))
